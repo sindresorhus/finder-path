b91ab167bb3615b6320efe3349e3e200a8b12383
Sindre Sorhus <sindresorhus@gmail.com>
2014-01-11 02:59:29 +0100

    trim whitespace on returned path

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ var execFile = require('child_process').execFile;
 module.exports = function (cb) {
 	var script = 'tell app "Finder" to POSIX path of (insertion location as alias)';
 
-	execFile('osascript', ['-e', script], function (err, stdout, stderr) {
-		cb(err, stdout);
+	execFile('osascript', ['-e', script], function (err, stdout) {
+		if (err) {
+			return cb(err);
+		}
+
+		cb(err, stdout.trim());
 	});
 };
