e031f5a9fa83d09d9de4775acc623d6df453af10
Sindre Sorhus <sindresorhus@gmail.com>
2015-10-07 23:30:16 +0700

    tweaks

--- index.js
+++ index.js
@@ -2,7 +2,5 @@
 var runApplescript = require('run-applescript');
 
 module.exports = function () {
-	var script = 'tell app "Finder" to POSIX path of (insertion location as alias)';
-
-	return runApplescript(script);
+	return runApplescript('tell app "Finder" to POSIX path of (insertion location as alias)');
 };
